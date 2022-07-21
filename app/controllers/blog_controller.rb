16b0edfee49da87a1ad157ca723aba2f2b730eeb
scharlez <stevencharlez@gmail.com>
2022-07-21 11:55:17 -0700

    App has update/edit/delete functionality

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,25 +1,42 @@
 class BlogController < ApplicationController
 
-def index 
+  def index 
     @blogs = Blog.all 
-end 
+  end 
 
-def show 
+  def show 
     @blog = Blog.find(params[:id])
-end 
+  end 
 
-def new
+  def new
     @blog = Blog.new
-end
+  end
 
-def create
+  def create
     @blog = Blog.create(blog_params)
     if @blog.valid?
       redirect_to blogs_path
-    else
-      redirect_to new_blog_path
     end
   end
+  
+  def destroy
+    @blog = Blog.find(params[:id])
+    if @blog.destroy
+      redirect_to blogs_path
+    end
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end 
+
+  def update 
+    @blog = Blog.find(params[:id])
+    @blog.update(blog_params)
+    if @blog.valid?
+      redirect_to blogs_path(@blog)
+    end
+  end 
 
   private 
   def blog_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get 'blogs' => 'blog#index', as: 'blogs'
   get 'blogs/new' => 'blog#new', as: 'new_blog'
-  get 'blogs/:id' => 'blog#show', as: 'blog'
   post 'blogs' => 'blog#create'
+  get 'blogs/:id' => 'blog#show', as: 'blog'
+  get 'blogs/:id/edit' => 'blog#edit', as: 'edit_blog'
+  patch 'blogs/:id' => 'blog#update'
+  delete 'blogs/:id' => 'blog#destroy', as: 'delete_blog'
 end
